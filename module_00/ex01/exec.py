b2486e118e45f1988d03ab80c676565b66cf1a16
Xavier Rodriguez-Rubi Alonso <xavirodriguezrubi@gmail.com>
2023-03-13 18:37:59 +0100

    module00 ex01 completed (update)

--- module_00/ex01/exec.py
+++ module_00/ex01/exec.py
@@ -1,6 +1,7 @@
 import sys
 
-string = ' '.join(sys.argv[1:])
-inv_string_swapcase = string[::-1].swapcase()
+#string = ' '.join(sys.argv[1:])
+#inv_string_swapcase = string[::-1].swapcase()
+#print(inv_string_swapcase)
 
-print(inv_string_swapcase)
+print(' '.join(sys.argv[1:])[::-1].swapcase())
