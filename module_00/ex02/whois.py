8f8eb91fc96c6fb4106a44b9b48b001312a9c144
Xavier Rodriguez-Rubi Alonso <xavirodriguezrubi@gmail.com>
2023-03-13 19:37:34 +0100

    module00 ex02 completed

---
+++ module_00/ex02/whois.py
@@ -0,0 +1,20 @@
+import sys
+
+argv = sys.argv[:]
+
+def who_is(num):
+    if num == 0:
+        print("I'm Zero.")
+    elif num % 2:
+        print("I'm Odd.")
+    else:
+        print("I'm Even.")
+
+if len(argv) > 2:
+    print("AssertionError: more than one argument are provided")
+elif len(argv) < 2:
+    print("AssertionError: less than one argument are provided")
+elif not argv[1].isdigit():
+    print("AssertionError: argument is not an integer")
+else:
+    who_is(int(argv[1]))
