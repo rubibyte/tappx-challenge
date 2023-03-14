b9f6b2b71c3505153a555cb7d5c880424160c9d3
Xavier Rodriguez-Rubi Alonso <xavirodriguezrubi@gmail.com>
2023-03-14 18:33:03 +0100

    module00 ex07 completed

---
+++ module_00/ex07/filterwords.py
@@ -0,0 +1,27 @@
+import sys
+import re
+
+
+argv = sys.argv
+
+
+def input_rules():
+    print("Enter two arguments as input: a string and an integer.")
+
+
+if len(argv) > 3:
+    print("AssertionError: too many arguments.")
+    input_rules()
+elif len(argv) < 3:
+    print("AssertionError: too few arguments.")
+    input_rules()
+elif not argv[2].isdigit():
+    print("AssertionError: second argument is not an integer.")
+    input_rules()
+else:
+    no_punctuation_str = re.sub(r'[^\w\s]', '', argv[1])
+    result = list()
+    for word in no_punctuation_str.split():
+        if len(word) > int(argv[2]):
+            result.append(word)
+    print(result)
