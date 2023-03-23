69ccb51ac53ce0aee6c7a25ac884a4b538462ae2
Xavier Rodriguez-Rubi Alonso <xavirodriguezrubi@gmail.com>
2023-03-23 22:18:28 +0100

    new tests added

--- hiring_project/json_reader.py
+++ hiring_project/json_reader.py
@@ -86,6 +86,9 @@ for i in range(0, len(video_ids)):
 # articulo de nombres de bebes, 12, 34 i 35
 
 
+# ultim article amb 30, 5, 29, 31
+
+
 # print(' '.join(video_keywords("pre-b42video32")))
 # print(keybert_keywords(video_text(video_ids[0])))
 
